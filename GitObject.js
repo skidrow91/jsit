bb3860737fe09056be46d46b82e3c08bcffa3d1a
Han Le (Alexi) <vanhan91@gmail.com>
2021-03-11 16:21:25 +0700

    Update GitObject.js

--- GitObject.js
+++ GitObject.js
@@ -1,11 +1,20 @@
 const crypto = require('crypto')
+const zlib = require('zlib')
+const fs = require('fs')
 
 class GitObject {
 
   hashObject(data) {
     let hash = crypto.createHash('sha1')
-    data = hash.update('data')
-    data.digest('hex')
+    // data = hash.update(data, 'utf-8')
+    let hashString = hash.update(data).digest('hex')
+    return hashString
+  }
+
+  createFile(hash, data) {
+    let dirName = hash.substr(0, 2)
+    let fileName = hash.substr(2)
+
   }
 }
 
