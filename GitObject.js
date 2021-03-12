4f5d9c256511c69637a363a974bcc86837051457
Han Le (Alexi) <vanhan91@gmail.com>
2021-03-12 10:54:35 +0700

    Update GitObject.js

--- GitObject.js
+++ GitObject.js
@@ -11,10 +11,33 @@ class GitObject {
     return hashString
   }
 
-  createFile(hash, data) {
+  async createFile(hash, data) {
+    let path = './.git/objects'
     let dirName = hash.substr(0, 2)
     let fileName = hash.substr(2)
+    // let str = zlib.deflateSync(data).toString('utf8')
+    let dirPath = path + '/' + dirName
+    console.log(dirPath)
+    let fileExist = await this.checkFileExist(dirPath)
 
+    if (fileExist == false) {
+      fs.mkdirSync(dirPath)
+    }
+
+    let filePath = dirPath + '/' + fileName
+    fs.writeFileSync(filePath, zlib.deflateSync(data))
+  }
+
+  checkFileExist(filename) {
+    return new Promise((resolve, reject) => {
+      fs.access(filename, fs.constants.F_OK, (err) => {
+        if (err) {
+          resolve(false)
+        } else {
+          resolve(true)
+        }
+      })
+    })
   }
 }
 
